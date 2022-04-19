502472f6ae277f4ecdecde1ab1e889d8725ef7aa
cehao <cehao@outlook.com>
2022-04-19 16:43:11 +0800

    good job, ready for lab

--- reference/get_pred_traj.m
+++ reference/get_pred_traj.m
@@ -1,4 +1,7 @@
 function [p_pred, v_pred, a_pred] = get_pred_traj(t, wave_pattern, A, omega)
+    p_pred = 0;
+    v_pred = 0;
+    a_pred = 0;
     if (wave_pattern == 0)
         p_pred = A;
         v_pred = 0;
